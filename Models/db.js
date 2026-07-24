ccd9433d99cbcf9a14eaffb7e2d97b648a3899f4
27abhishekcodes <27abhishekcool@gmail.com>
2026-07-24 17:11:43 +0530

    Update db.js

--- Models/db.js
+++ Models/db.js
@@ -1,35 +1,24 @@
 const mongoose = require("mongoose");
 
-let isConnected = false;
-
 const connectDB = async () => {
-    if (isConnected) {
-        return;
-    }
-
     try {
-        console.log("MONGO_CONN exists:", !!process.env.MONGO_CONN);
+        // Already connected
+        if (mongoose.connection.readyState === 1) {
+            console.log("MongoDB already connected");
+            return;
+        }
+
         console.log("Before connect:", mongoose.connection.readyState);
-        const connection = await mongoose.connect(
-  "mongodb+srv://abhishek_1234:Abhishek1234@cluster0.nj7buny.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
+
+        await mongoose.connect(
+            "mongodb+srv://abhishek_1234:Abhishek1234@cluster0.nj7buny.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0"
         );
 
         console.log("After connect:", mongoose.connection.readyState);
-console.log("Connected to:", connection.connection.host);
-
-        isConnected = true;
-
-        console.log(
-            "MongoDB Connected:",
-            connection.connection.host
-        );
+        console.log("Connected to:", mongoose.connection.host);
 
     } catch (error) {
-        console.error(
-            "MongoDB Connection Error:",
-            error.message
-        );
-
+        console.error("MongoDB Connection Error:", error);
         throw error;
     }
 };
