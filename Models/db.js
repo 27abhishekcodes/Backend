8b388d0657e1155cb867a5c5e39509376e51db1e
27abhishekcodes <27abhishekcool@gmail.com>
2026-07-24 13:11:51 +0530

    Update db.js

--- Models/db.js
+++ Models/db.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-const mongo_url = process.env.MONGO_CONN;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_CONN);
+        console.log("MongoDB Connected...");
+    } catch (err) {
+        console.log("MongoDB Error:", err.message);
+    }
+};
 
-console.log("Mongo URL exists:", !!mongo_url);
-
-mongoose.connect(mongo_url)
-    .then(() => {
-        console.log("✅ MongoDB Connected");
-    })
-    .catch((err) => {
-        console.error("❌ MongoDB Connection Error:", err.message);
-    });
+module.exports = connectDB;
